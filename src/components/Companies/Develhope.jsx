2f462df833c0515b535d4cf8b2e52df2d160dc30
Davide Ricca <davidericca109@gmail.com>
2025-03-06 11:02:30 +0100

    fix jobs

--- src/components/Companies/Develhope.jsx
+++ src/components/Companies/Develhope.jsx
@@ -4,7 +4,7 @@ export const Develhope = ({ theme }) => {
     const { t } = useTranslation();
     return (
         <div className={`py-1 px-2 exp-single ${theme}`}>
-            <h4 className="py-1">UniWix <span className="ml-3">Tutor Full-Stack</span></h4>
+            <h4 className="py-1">Develhope <span className="ml-3">Tutor Full-Stack</span></h4>
             <h5>03 / 2024 - 09 / 2024</h5>
             <div className="py-2">
                 <p>{t("experience.develhope")}</p>
